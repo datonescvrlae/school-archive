0c934ca702c69ddc140e776ca0f2706f97183506
Scvrlae <yurmepsycliv@gmail.com>
2025-09-02 16:36:46 +0200

    Scvrlae commit

---
+++ C++/02.09.2025 - Liczby Parzyste z For Loop/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+       unsigned int n; // Tworzymy nowa pusta zmienna o nazwie "n" typu "int" czyli liczba
+
+       // Najpierw wypisujemy w konsoli nasz tekst za pomoca "cout"
+       // Warto zapamietac ze strzalki musza w tym przypadku byc w strone slowa "cout"
+
+       // Pozniej dajemy uzytkownikowi opcje wpisania liczby ktora zostanie przypisana do
+       // naszej zmiennej "n" zdeklarowanej wyzej w linii 6
+       // Tutaj tez warto zapamietac ze strzalki musza byc w PRZECIWNA STRONE niz slowo "cin"
+
+       // A zeby zapamietac same slowa "cout" oraz "cin" wystarczy je rozdzielic np. cout to "c" + "out"
+       cout << "Podaj ile liczb parzystych chcesz wyswietlic: ";
+       cin >> n;
+
+       // Petle for mozna podzielic na 3 glowne elementy: "[zmienne]; [warunki]; [zmiany po petli]"
+       
+       // W przypadku tej petli tworzymy dwie zmienne "i" oraz "j" ODDZIELONE PRZECINKIEM!!
+       // Naszym warunkiem jest to ze dopoki "i" jest mniejsze niz "n" (ktore uzytkownik podal wyzej w "cin") petla dziala
+       // I na koniec po kazdej iteracji (czyli powtorzeniu) zwiekszamy "i" o 1 oraz "j" o 2
+
+       // Ponizej podaje przyklad dla n = 3
+       // Iteracja 1:
+       //     i = 0
+       //     j = 0
+
+       // Iteracja 2:
+       //     i = 1
+       //     j = 2
+
+       // Iteracja 3:
+       //     i = 2
+       //     j = 4
+
+       // Iteracja 4:
+       //     i = 4 czyli ta petla juz sie nie wykona bo nasz warunek to "i" jest mniejsze od "n" (czyli w tym przykladzie 3)
+       for (int i = 0, j = 0; i < n; i++, j+=2) 
+       {
+              cout << j << " "; // Wypisujemy nasza zmienna "j" i dodajemy po niej spacje
+       }
+
+       cout << endl; // "endl" to nic innego jak po prostu pusta linia
+
+       // Na samym koncu jezyk C++ wymaga od nas napisania "return 0;"
+       // Nie wazne dlaczego po prostu trzeba o tym pamietac
+       return 0;
+}
